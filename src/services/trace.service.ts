9f297cb258ccd78a7e59e4101ae8893519ac2d6a
Militao36 <matheusmoreira3693@gmail.com>
2025-11-13 11:41:04 -0300

    Adicionado config

--- src/services/trace.service.ts
+++ src/services/trace.service.ts
@@ -1,7 +1,9 @@
 import { Queue } from "bull";
-import { NormalizedSpan } from "../queues/bull/utils/normalizeOtlpHttpJsonTrace";
 import { RedisClientType } from "@redis/client";
 import { Logger } from "pino";
+
+import { NormalizedSpan } from "../queues/bull/utils/normalizeOtlpHttpJsonTrace";
+import { LIMIT_SPANS_QUEUE_DEFAULT } from "../env";
 
 export class TracesService {
   queueTraces: Queue
@@ -9,13 +11,13 @@ export class TracesService {
   clientRedis: RedisClientType
   logger: Logger
 
-  LIMIT_SPANS_QUEUE: number = 1000
+  LIMIT_SPANS_QUEUE: number
 
   constructor({ queueTraces, logger, clientRedis, normalizeOTLP }) {
     this.queueTraces = queueTraces
     this.normalizeOTLP = normalizeOTLP
     this.clientRedis = clientRedis
-    this.LIMIT_SPANS_QUEUE = 1000
+    this.LIMIT_SPANS_QUEUE = LIMIT_SPANS_QUEUE_DEFAULT
     this.logger = logger
   }
 
@@ -26,9 +28,6 @@ export class TracesService {
 
     const redisKey = `trace_count:${idEmpresa}`;
     const listSpans = await this.clientRedis.incr(redisKey);
-
-
-    this.logger.info(`Current span count for company ${idEmpresa} is ${listSpans}`);
 
     if (listSpans >= this.LIMIT_SPANS_QUEUE) {
       this.logger.info(`Limit of ${this.LIMIT_SPANS_QUEUE} spans reached for company ${idEmpresa}, sending to queue`);
@@ -43,6 +42,7 @@ export class TracesService {
       if (parsedSpans.length > 0) {
         await this.clientRedis.del(`trace_spans:${idEmpresa}`);
         await this.clientRedis.set(redisKey, '0');
+        
         await this.queueTraces.add({
           idEmpresa,
           spans: parsedSpans,
