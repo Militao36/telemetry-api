3dfff36819ec4e941ea1f7253bd9ca75f1ead1d7
Militao36 <matheusmoreira3693@gmail.com>
2025-11-28 08:11:19 -0300

    fix

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -7,7 +7,7 @@ import { UserService } from '../services/user.service';
 import { ProjectService } from '../services/project.service';
 
 export async function auth(req: Request, res: Response, next: NextFunction): Promise<any> {
-  const excludes = ['/api/v1/users','/api/v1/users/auth'];
+  const excludes = ['/api/v1/users', '/api/v1/users/auth'];
 
   if (excludes.includes(req.url)) {
     next();
